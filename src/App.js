44d3f9f1c921374aeae6420edd393d2fcf3b5920
JhoellOpeyemi <jhoellpelumi@gmail.com>
2022-01-22 14:24:19 +0100

    fixed nav overflow on mobile

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 
-// import media queries
-import "./styles/media.css";
-
 function App() {
   return (
     <div className="container">
